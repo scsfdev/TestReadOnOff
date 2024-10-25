2d0176d45d5800c6c56493c1b8c1ac5a0e4cee17
Tin Maung Htay <seainfu@hotmail.com>
2024-10-25 11:51:02 +0800

    Add project files.

---
+++ TestReadOnOff/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestReadOnOff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
